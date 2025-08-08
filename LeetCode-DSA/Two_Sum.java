1252e1e708e5e382275795a0b6d8e2869621f418
Poorvika N P <poorvikanp245@gmail.com>
2025-08-08 15:44:13 +0530

    Initial commit with solutions

---
+++ LeetCode-DSA/Two_Sum.java
@@ -0,0 +1,26 @@
+public class Two_Sum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        
+        int[] result = twoSum(nums, target);  // call static method
+        
+        if (result.length > 0) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two sum solution found.");
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) { // make static
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // empty if no pair found
+    }
+}
